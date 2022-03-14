874f66dec576dd59cb72dbd3055f90ecfb6c8564
Vojtěch Dolák <dolakv.05@spst.cz>
2022-03-14 09:16:20 +0100

    P01 hotovo

--- Pole2/P01/Form1.cs
+++ Pole2/P01/Form1.cs
@@ -19,7 +19,91 @@ namespace P01
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+            
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            Random rnd = new Random();
+            int min = 1000;
+            int max = 0;
+            int q = 0;
+            int pozicemax = 0;
+            int pozicemin = 0;
+            double soucet = 0;
+            int pocet = 0;
+            
+            int n = Convert.ToInt32(textBox1.Text);
+            int[] pole = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                pole[i] = rnd.Next(1, 1000);
+                soucet += pole[i];
+             
+                if (pole[i] < min)
+                {
+                    min = pole[i];
+                    pozicemin = i;
+                }
+                if (pole[i] > max)
+                {
+                    max = pole[i];
+                    pozicemax = i;
+                }
+            }
+            foreach (int s in pole)
+                listBox1.Items.Add(s);
+            pole[pozicemin] = max;
+            pole[pozicemax] = min;
+            if(pocet > 0)
+            {
+                double prumer = (soucet - min - max) / (pocet - 2);
+                MessageBox.Show("prumer cisel v poli bez minima a maxima je " + prumer);
+            }
+            foreach (int s in pole)
+                listBox2.Items.Add(s);
+            
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            Random rnd = new Random();
+            int min = 1000;
+            int max = 0;
+            int q = 0;
+            double soucet = 0;
+            int pocet = 0;
+
+            int n = Convert.ToInt32(textBox1.Text);
+            int[] pole = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                pole[i] = rnd.Next(1, 1000);
+            }
+            soucet = pole.Sum();
+            min = pole.Min();
+            max = pole.Max();
+            foreach (int s in pole)
+                listBox1.Items.Add(s);
+            int pozicemin = Array.IndexOf(pole, min);
+            int pozicemax = Array.IndexOf(pole, max);
+            pole[pozicemin] = max;
+            pole[pozicemax] = min;
+            if (pocet > 0)
+            {
+                double prumer = (soucet - min - max) / (n  - 2);
+                MessageBox.Show("prumer cisel v poli bez minima a maxima je " + prumer);
+            }
+            foreach (int s in pole)
+                listBox2.Items.Add(s);
         }
     }
 }
