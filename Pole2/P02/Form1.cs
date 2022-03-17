272e6255f5d1a3fdf3984750afadd27f200501b2
Vojtěch Dolák <dolakv.05@spst.cz>
2022-03-17 10:43:13 +0100

    ........

--- Pole2/P02/Form1.cs
+++ Pole2/P02/Form1.cs
@@ -24,23 +24,36 @@ namespace P02
         }
         private void button1_Click(object sender, EventArgs e)
         {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
             Random rnd = new Random();
             int n = Convert.ToInt32(textBox1.Text);
             int[] pole = new int[n];
+            int pozicemin2 = 0;
+            int pozicemax2 = 0;
+            int max2;
+            int min2;
             for (int i = 0; i < n; i++)
             {
                 pole[i] = rnd.Next(1, 11);
-
             }
             foreach (int s in pole)
                 listBox1.Items.Add(s);
             Array.Sort(pole);
+            int max = pole[n - 1];
+            int min = pole[0];
+            pozicemax2 = (Array.IndexOf(pole, max)) - 1;
+            pozicemin2 = (Array.LastIndexOf(pole, min)) + 1;
+            min2 = pole[pozicemin2];
+            max2 = pole[pozicemax2];
             if (radioButton2.Checked == true)
             {
                 Array.Reverse(pole);
             }
             foreach (int s in pole)
                 listBox2.Items.Add(s);
+            MessageBox.Show("druhy nejvetsi cislo je " + max2);
+            MessageBox.Show("druhy nejmensi cislo je " + min2);
         }
 
     }
